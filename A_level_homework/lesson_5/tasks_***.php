d2efa35ca55493f40f0f1a6bab58013e2b058ca3
valerka <rhfcfdxbrt@gmail.com>
2021-10-24 18:15:13 +0300

    refresh task***

--- A_level_homework/lesson_5/tasks_***.php
+++ A_level_homework/lesson_5/tasks_***.php
@@ -112,7 +112,7 @@ for ($i = 0; $i >= 0; $i++) {
 }
 $arr1 = [];
 for ($q = $count; $q >= 0; $q--) {
-    $arr1[] = $arr[$q] ;
+    $arr1[] = $arr[$q];
 }
 print_r($arr1);
 
@@ -235,19 +235,18 @@ do {
 } while (true);
 
 #for
-$count = 0;
-for ($i = 0; $i >= $count; $i++) {
-    if ($txt[$count] == '') {
+
+for ($i = 0; $i >= $i; $i++) {
+    if ($txt[$i] == '') {
         break;
     }
     for ($p = 0; $p >= 0; $p++) {
         if ($str_up[$p] == '') {
             break;
-        } elseif ($txt[$count] == $str_up[$p] or $txt[$count] == $str_low[$p]) {
-            echo $txt[$count] = $str_low[$p];
+        } elseif ($txt[$i] == $str_up[$p] or $txt[$i] == $str_low[$p]) {
+            echo $txt[$i] = $str_low[$p];
         }
     }
-    $count++;
 }
 
 // foreach пока нету , не могу понять как им два массива сравнить.
@@ -356,12 +355,13 @@ for ($i = 0; $i >= 0; $i++) {
 }
 $str1 = '';
 for ($q = $count; $q >= 0; $q--) {
-    $str1 .= $str[$q] ;
+    $str1 .= $str[$q];
 }
 echo $str1;
 
 #foreach
-for ($i = 0; $i >= 0; $i++) { // нужно строку в массив переделать
+// нужно строку в массив переделать
+for ($i = 0; $i >= 0; $i++) {
     if ($str[$i] == '') {
         break;
     }
@@ -377,7 +377,466 @@ foreach ($new_str as $k) {
     echo $new_arr[] = $new_str[$count];
 }
 
-
 //7. Дан массив
 //['Alex', 'Vanya', 'Tanya', 'Lena', 'Tolya']
 //сделать все буквы с маленькой
+$arr = ['ALEx', 'Vanya', 'Tanya', 'Lena', 'Tolya'];
+
+$str_low = ' abcdefghijklmnopqrstuvwxyz';
+$str_up = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+#while
+$count = 0;
+$str_arr = '';
+$empty = ' ';
+while (true) {
+    if ($arr[$count] == '') {
+        break;
+    }
+    $str_arr .= $arr[$count];
+    $str_arr .= $empty;
+    $count++;
+}
+//var_dump($str_arr);
+$y = 0;
+while (true) {
+    if ($str_arr[$y] == '') {
+        break;
+    }
+    while (true) {
+        if ($str_up[$x] == '') {
+            $x = 0;
+            break;
+        } elseif ($str_arr[$y] == $str_low[$x] or $str_arr[$y] == $str_up[$x]) {
+            $str_arr[$y] = $str_low[$x];
+        }
+        $x++;
+    }
+    $y++;
+}
+$newArr = explode(' ', $str_arr);
+print_r($newArr);
+
+# А здесь я пытался что то сделать со строкой
+# обратно в тот же массив дефолтный но без результатно как я понял
+# 3 часа я долбался с этой задачей , и видимо я рано сдался и поддался на искушение explode`a =)
+# Ну или же еще не хватает знаний
+//var_dump($str_arr);
+//$q = 0;
+//$z = 0;
+//while (true){
+//    if ($str_arr[$q] == ''){
+//        break;
+//    }
+//    if ($str_arr[$q] !== '.'){
+//       $new_arr[] = $str_arr[$q];
+//    }
+//    $q++;
+//}
+//echo '<pre>';
+//print_r($new_arr);
+//echo '</pre>';
+
+
+#do_while
+$empty = ' ';
+$count = 0 ;
+$strArr = '';
+
+do{
+    if ($arr[$count] == ''){
+        break;
+    }
+    $strArr .= $arr[$count];
+    $strArr .= $empty;
+    $count++;
+}while (true);
+
+//var_dump($strArr);
+
+$y = 0;
+do{
+    if ($strArr[$y] == '') {
+        break;
+    }
+    do{
+        if ($str_up[$x] == '') {
+            $x = 0;
+            break;
+        } elseif ($strArr[$y] == $str_low[$x] or $strArr[$y] == $str_up[$x]) {
+            $strArr[$y] = $str_low[$x];
+        }
+        $x++;
+    }while(true);
+    $y++;
+}while(true);
+
+$newArr = explode(' ', $strArr);
+print_r($newArr);
+
+#for
+$strArr = '';
+$empty = ' ';
+
+for ($i = 0; $i >= 0 ; $i++){
+    if ($arr[$i] == ''){
+        break;
+    }
+    $strArr .= $arr[$i];
+    $strArr .= $empty;
+}
+//var_dump($strArr);
+for ($i = 0; $i >= 0 ; $i++){
+    if ($strArr[$i] == '') {
+        break;
+    }
+    for ($p = 0; $p >= 0; $p++) {
+        if ($str_up[$p] == '' ) {
+            break;
+        } elseif ($strArr[$i] == $str_up[$p] or $strArr[$i] == $str_low[$p]) {
+            $strArr[$i] = $str_low[$p];
+        }
+    }
+
+}
+
+$newArr = explode(' ', $strArr);
+print_r($newArr);
+
+#foreach
+$strArrUp = [];
+$strArrLow = [];
+#
+# строки из букв конвертирую в массивы
+#
+for ($i = 0; $i >= 0; $i++) {
+    if ($str_low[$i] == '') {
+        break;
+    }
+    $strArrLow[] = $str_low[$i];
+}
+for ($i = 0; $i >= 0; $i++) {
+    if ($str_up[$i] == '') {
+        break;
+    }
+    $strArrUp[] = $str_up[$i];
+}
+
+# foreach перебираю массивы
+
+$strArr = '';
+$empty = ' '; # добавляю разделение в строке пробелом =)
+$newArr = [];
+
+# из массива делаю строку
+foreach ($arr as $key){
+    $strArr .= $key ;
+    $strArr .= $empty ;
+
+}
+//var_dump($strArr);
+
+# из строки делаю опять массив с ключами значениями  ( 0 => A и т.д.)
+
+for ($i = 0; $i >= 0; $i++) {
+    if ($strArr[$i] == '') {
+        break;
+    }
+    $newStrArr[] = $strArr[$i];
+}
+//echo '<pre>';
+//print_r($newStrArr);
+//echo '</pre>';
+#
+# обхожу массив и в пустую строку складываю все уже в нижнем регистре
+#
+$emptyStr = '';
+foreach ($newStrArr as $items ) {
+    for ($i = 0; $i >= 0 ; $i++){
+        if ($str_low[$i] == ''){
+            break;
+        }elseif ($str_up[$i] == $items or $str_low[$i] == $items){
+            $items = $str_low[$i];
+            $emptyStr .= $items;
+        }
+    }
+}
+
+$lastArr = explode(' ', $emptyStr);  #   возвращаю масиву исходный вид .
+//print_r($lastArr);
+
+
+
+//8. Дан массив
+//['Alex', 'Vanya', 'Tanya', 'Lena', 'Tolya']
+//сделать все буквы с большой
+$arr = ['ALEx', 'Vanya', 'Tanya', 'Lena', 'Tolya'];
+$str_low = ' abcdefghijklmnopqrstuvwxyz';
+$str_up = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+
+#while
+$count = 0;
+$str_arr = '';
+$empty = ' ';
+while (true) {
+    if ($arr[$count] == '') {
+        break;
+    }
+    $str_arr .= $arr[$count];
+    $str_arr .= $empty;
+    $count++;
+}
+//var_dump($str_arr);
+$y = 0;
+while (true) {
+    if ($str_arr[$y] == '') {
+        break;
+    }
+    while (true) {
+        if ($str_up[$x] == '') {
+            $x = 0;
+            break;
+        } elseif ($str_arr[$y] == $str_low[$x] or $str_arr[$y] == $str_up[$x]) {
+            $str_arr[$y] = $str_up[$x];
+        }
+        $x++;
+    }
+    $y++;
+}
+$newArr = explode(' ', $str_arr);
+print_r($newArr);
+
+#do_while
+$empty = ' ';
+$count = 0 ;
+$strArr = '';
+
+do{
+    if ($arr[$count] == ''){
+        break;
+    }
+    $strArr .= $arr[$count];
+    $strArr .= $empty;
+    $count++;
+}while (true);
+
+//var_dump($strArr);
+
+$y = 0;
+do{
+    if ($strArr[$y] == '') {
+        break;
+    }
+    do{
+        if ($str_up[$x] == '') {
+            $x = 0;
+            break;
+        } elseif ($strArr[$y] == $str_low[$x] or $strArr[$y] == $str_up[$x]) {
+            $strArr[$y] = $str_up[$x];
+        }
+        $x++;
+    }while(true);
+    $y++;
+}while(true);
+
+$newArr = explode(' ', $strArr);
+print_r($newArr);
+
+#for
+$strArr = '';
+$empty = ' ';
+
+for ($i = 0; $i >= 0 ; $i++){
+    if ($arr[$i] == ''){
+        break;
+    }
+    $strArr .= $arr[$i];
+    $strArr .= $empty;
+}
+//var_dump($strArr);
+for ($i = 0; $i >= 0 ; $i++){
+    if ($strArr[$i] == '') {
+        break;
+    }
+    for ($p = 0; $p >= 0; $p++) {
+        if ($str_up[$p] == '' ) {
+            break;
+        } elseif ($strArr[$i] == $str_up[$p] or $strArr[$i] == $str_low[$p]) {
+            $strArr[$i] = $str_up[$p];
+        }
+    }
+
+}
+
+$newArr = explode(' ', $strArr);
+print_r($newArr);
+
+
+#foreach
+$strArrUp = [];
+$strArrLow = [];
+#
+# строки из букв конвертирую в массивы
+#
+for ($i = 0; $i >= 0; $i++) {
+    if ($str_low[$i] == '') {
+        break;
+    }
+    $strArrLow[] = $str_low[$i];
+}
+for ($i = 0; $i >= 0; $i++) {
+    if ($str_up[$i] == '') {
+        break;
+    }
+    $strArrUp[] = $str_up[$i];
+}
+
+# foreach перебираю массивы
+
+$strArr = '';
+$empty = ' '; # добавляю разделение в строке пробелом =)
+$newArr = [];
+
+# из массива делаю строку
+foreach ($arr as $key){
+    $strArr .= $key ;
+    $strArr .= $empty ;
+
+}
+//var_dump($strArr);
+
+# из строки делаю опять массив с ключами значениями  ( 0 => A и т.д.)
+
+for ($i = 0; $i >= 0; $i++) {
+    if ($strArr[$i] == '') {
+        break;
+    }
+    $newStrArr[] = $strArr[$i];
+}
+//echo '<pre>';
+//print_r($newStrArr);
+//echo '</pre>';
+//#
+# обхожу массив и в пустую строку складываю все уже в нижнем регистре
+#
+$emptyStr = '';
+foreach ($newStrArr as $items ) {
+    for ($i = 0; $i >= 0 ; $i++){
+        if ($str_low[$i] == ''){
+            break;
+        }elseif ($str_up[$i] == $items or $str_low[$i] == $items){
+            $items = $str_up[$i];
+            $emptyStr .= $items;
+        }
+    }
+}
+
+$lastArr = explode(' ', $emptyStr);  #   возвращаю масиву исходный вид .
+//print_r($lastArr);
+
+
+//9. Дано число
+//let num = 1234678
+//развернуть ее в обратном направлении
+
+#while
+$num = 12346789;
+$str = '';
+$strNum = $str .= $num;
+$count = 0;
+$strReverse = '';
+while(true){
+    if ($strNum[$count] == '') {
+        break;
+    }
+    $count++;
+}
+while ($count-- >= 1) {
+    $str1 = $strNum[$count];
+    $strReverse .= $str1;
+}
+
+$qwerty = (int)$strReverse;
+var_dump($qwerty);
+
+#do_while
+$num = 12346789;
+$str = '';
+$strNum = $str .= $num;
+$count = 0;
+$strReverse = '';
+
+do{
+    if ($strNum[$count] == '') {
+        break;
+    }
+    $count++;
+}while(true);
+
+do{
+    $str1 = $strNum[$count];
+    $strReverse .= $str1;
+}while ($count-- >= 1);
+
+
+$qwerty = (int)$strReverse;
+//var_dump($qwerty);
+
+
+#for
+$num = 12346789;
+$str = '';
+$strNum = $str .= $num;
+$count = 0;
+$strReverse = '';
+
+
+for ($i = 0;$i >= 0;$i++){
+    if ($strNum[$i] == '') {
+        break;
+    }
+    $count++;
+}
+for ($i = $count ; $i > 0; $i--){
+    $str1 = $strNum[$i];
+    $strReverse .= $str1;
+}
+
+$qwerty = (int)$strReverse;
+//var_dump($qwerty);
+
+#foreach
+$num = 123456789;
+$str = '';
+$strNum = $str .= $num;
+$count = 0;
+$arrNum = [];
+
+#создаю массив из строки чисел
+while (true) {
+    if ($strNum[$count] == '') {
+//        $count = 0;
+        break;
+    }
+    $arrNum[] = $strNum[$count];
+    $count++;
+}
+//echo $count;
+//print_r($arrNum);
+
+#раскладываю массив в обратном порядке
+$arrReverse = [];
+foreach ($arrNum as $v) {
+    $count--;
+    $arrReverse[] = $arrNum[$count];
+}
+
+#конвертирую массив обратно в строку обратного порядка
+$newStr = '';
+foreach ($arrReverse as $value){
+    $newStr .= $value;
+}
+
+$newNum =  (int)$newStr; #а здесь уже привожу строку к числу.
+var_dump($newNum);
